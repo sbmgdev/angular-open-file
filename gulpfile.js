1fd9bfcb1ef82b836321bbb76769676a8d27cd16
Diego Gurgel <diego.gurgel@sebraemg.com.br>
2016-09-20 13:17:56 -0300

    uglify fail after builds in projects

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ let rename = require('gulp-rename');
 gulp.task('default',()=>{
 	return gulp.src('angular-open-file.js')
 		.pipe(babel())
-		.pipe(uglify())
-		.pipe(rename('angular-open-file.min.js'))
+		.pipe(rename('angular-open-file.js'))
 		.pipe(gulp.dest('dist'))
 })
