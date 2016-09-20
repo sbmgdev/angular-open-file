111cac4a31af7b13407c9bce6591abdb0c1feb59
Diego Gurgel <diego.gurgel@sebraemg.com.br>
2016-09-20 11:26:47 -0300

    run babel

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+let gulp = require('gulp');
+let babel = require('gulp-babel');
+let uglify = require('gulp-uglify');
+let rename = require('gulp-rename');
+
+gulp.task('default',()=>{
+	return gulp.src('angular-open-file.js')
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(rename('angular-open-file.min.js'))
+		.pipe(gulp.dest('dist'))
+})
